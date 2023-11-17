f2064646fe9453048488ea1b3590e3bd32970623
migueelzz <miguellemes00@gmail.com>
2023-11-17 09:16:08 -0300

    connect with mongo

--- src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://mongo/mongodb')
-.then(db => console.log('Successfully connected in database', db.connection.host))
-.catch(err => console.log(err));
